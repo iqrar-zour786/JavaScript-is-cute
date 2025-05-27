0e04cd117721f8f26554777ecf1ff88bde1c5c72
Iqrar Ali <iqrarzour41@gmail.com>
2025-05-27 13:12:27 +0000

    DataTypes

---
+++ data_types.js
@@ -0,0 +1,43 @@
+
+
+"use strict"; // it means all the codes are in new engines..
+
+// alert ("Hello Developers 😎😎😎😎"); // a new pop-up in the front of websites.
+
+console.log(6+3) // code readability should be more high.
+
+console.log("Hello")
+
+
+let words = "javascript"
+let age = 20
+let number ;
+
+
+/*
+numbers = 2 to the power 53
+strings = ""
+undefined = means value is not assigned yet..
+null => standalone value
+bigint = extra large big number..
+boolean = true/false
+symbol = unique
+
+these are primitive datatypes of javascript.
+*/
+
+
+// OBJECT
+
+console.log(typeof "iqrar");
+
+console.log(typeof 10);
+
+console.log(typeof number);
+
+console.log(typeof age);
+
+console.log(typeof null); // this is an object of javascript.
+
+console.log(typeof undefined); // undefined value.
+
