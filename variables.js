3b2a4736c485371b9f92fedba444eacbe8ab3011
Iqrar Ali <iqrarzour41@gmail.com>
2025-05-27 11:14:22 +0000

    dev containers

---
+++ variables.js
@@ -0,0 +1,13 @@
+
+const accountID = 1122334455
+let accountEmail = "zouriqrar36@google.com"
+var accountPassword = "12345"
+accountCity = "Makli"
+
+
+
+// console.log(accountID);
+
+
+console.table([accountEmail, accountID, accountPassword,accountCity]);
+
