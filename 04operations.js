69f97ea0589adae9d15d54a5d78604f3490161fc
Iqrar Ali <iqrarzour41@gmail.com>
2025-06-02 20:09:17 +0000

    operations

---
+++ 04operations.js
@@ -0,0 +1,56 @@
+
+
+
+
+let value = 3
+let negvalue = -value
+console.log(negvalue)
+
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/2);
+console.log(2%2);
+
+
+let str1 = "Hello"
+let str2 = " Iqrar"
+let str3 = str1 + str2
+console.log(str3);
+
+console.log(1 + "3");
+console.log(1 + "3" + 2);
+console.log(1 + 3 + "3");
+console.log("1" + 3);
+console.log(1 + 3 * 2 % 2); //this method is not good ..
+console.log((2 +3) * 2 % 3); // this method is good for everything.
+
+console.log(1 + "3");
+console.log(1 + "3");
+
+console.log(true);
+
+console.log(+true);
+
+console.log(+false);
+
+console.log(+"");
+
+
+let num1 , num2, num3 // this is not a right way to write a code.
+num1 = num2 = num3 = 2 + 2 // this is not a right way to write a code.
+
+
+let gamecounter = 100
+gamecounter++;
+console.log(gamecounter);
+
+
+let gamecounters = 100
+++gamecounters;
+console.log(gamecounter);
+
+
+
