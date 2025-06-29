2187438cfe99d00b8508d26b06927c67b1a54914
Iqrar Ali <iqrarzour41@gmail.com>
2025-06-29 19:21:59 +0000

    Primitives

---
+++ primitives.js
@@ -0,0 +1,43 @@
+let score = 10;
+// console.log(typeof score);
+
+let balance = new Number(120);
+// console.log(balance.valueOf());
+
+let balances = 30;
+// console.log(typeof balances);
+// console.log(typeof balance);
+
+// BOOLEAN
+
+let isActive = true;
+let iseallyActive = new Boolean(true); // not recommended
+
+// NULL & UNDEFINED
+
+let firstName = null;
+let lastName = undefined;
+// console.log(firstName);
+// console.log(lastName);
+
+// String
+
+let myString = "Developer";
+let username = "iqrar";
+let last = "Hitesh";
+
+let oldGreet = myString + " Hitesh";
+// console.log(oldGreet);
+
+let greatMessage = `hello ${username} !`;
+// console.log(greatMessage);
+
+let demo = `value is ${2 * 2}`;
+// console.log(demo);
+
+// SYMBOL
+
+let sm1 = Symbol();
+let sm2 = Symbol();
+// console.log(sm1 == sm2);
+console.log(sm1);
