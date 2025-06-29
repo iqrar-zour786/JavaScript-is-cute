c2ec6d957a832fa9b621a0b3a2cc5fe93705ac8c
Iqrar Ali <iqrarzour41@gmail.com>
2025-06-29 19:25:18 +0000

    Primitives

---
+++ non-primitives.js
@@ -0,0 +1,43 @@
+// // NON-PRIMITIVES IN JS
+
+// // let code = { firstName: "Iqrar", isLogged: true }; THIS IS GOOD FOR WRITE A CODE
+
+// const username = {
+//   "first Name": "Iqrar",
+//   isLogged: true,
+// };
+
+// username.firstName = "Zour";
+// username.lastName = "Khan";
+
+// console.log(username["first Name"]);
+// console.log(username.lastName);
+// console.log(typeof username);
+
+// const gam = {
+//   game: "cyber",
+//   games: 120,
+// };
+
+// gam.game = "security";
+
+// console.log(gam.game);
+
+// console.log(username);
+
+// let today = new Date();
+// console.log(today.getDate());
+
+// ARRAY IN JS
+
+let user = ["iqrarZour036", "ZourKhan927", true];
+// console.log(user[(0, 1)]);
+
+// let isLoggedin = true;
+let isLoggedin = false;
+// console.log(isLoggedin + 1);
+
+// console.log(1 + 2 + "1");
+
+let score = "2abc";
+console.log(typeof Number(score));
--- primitives.js
+++ primitives.js
@@ -42,3 +42,5 @@ let sm1 = Symbol();
 let sm2 = Symbol();
 // console.log(sm1 == sm2);
 console.log(sm1);
+
+
